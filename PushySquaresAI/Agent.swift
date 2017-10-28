c14f0bf3074a7a62003737deab4e8fe4bf5ddef2
Sweeper777 <sumulang@gmail.com>
2017-10-28 09:54:43 +0100

    added Agent class properties

---
+++ PushySquaresAI/Agent.swift
@@ -0,0 +1,14 @@
+import RealmSwift
+
+class Agent: Object {
+    
+    dynamic var wSelfLife = 0
+    dynamic var wDiffLives = 0
+    dynamic var wSquareThreshold = 0
+    dynamic var wSelfSpreadBelowThreshold = 0
+    dynamic var wSelfSpreadAboveThreshold = 0
+    dynamic var wOpponentSpread = 0
+    dynamic var wSelfInDanger = 0
+    dynamic var wOpponentInDangerBelowThreshold = 0
+    dynamic var wOpponentInDangerAboveThreshold = 0
+}
