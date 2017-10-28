13b5b03e1dd68b184a5f98fb5a80b2e0fbea69e8
Sweeper777 <sumulang@gmail.com>
2017-10-28 09:56:43 +0100

    added init for AgentGame

--- PushySquaresAI/AgentGame.swift
+++ PushySquaresAI/AgentGame.swift
@@ -4,6 +4,21 @@ class AgentGame: GameDelegate {
     var fitness: [Agent: Int]
     let agents: [Agent]
     let game: Game
+    
+    init(agents: [Agent]) {
+        game = Game(map: .standard, playerCount: agents.count)
+        var tempColors: [Color: Agent] = [.color1: agents[0], .color3: agents[1]]
+        if agents.count > 2 {
+            tempColors[.color2] = agents[2]
+        }
+        if agents.count > 3 {
+            tempColors[.color4] = agents[3]
+        }
+        self.colors = tempColors
+        self.agents = agents
+        self.fitness = [:]
+        game.delegate = self
+    }
     func playerDidMakeMove(direction: Direction?, originalPositions: [Position], destroyedSquarePositions: [Position], greyedOutPositions: [Position], newSquareColor: Color?) {
         
     }
