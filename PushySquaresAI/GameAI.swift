9c931230d210c524d771804e1ec42ba95fb885b8
Sweeper777 <sumulang@gmail.com>
2017-10-29 07:44:08 +0000

    added Game extension opponents

--- PushySquaresAI/GameAI.swift
+++ PushySquaresAI/GameAI.swift
@@ -1,3 +1,4 @@
+
 extension Game {
     func createCopy() -> Game {
         return Game(game: self)
@@ -5,6 +6,10 @@ extension Game {
     
     func player(_ color: Color) -> Player {
         return players.filter { $0.color == color }.first!
+    }
+    
+    func opponents(to color: Color) -> [Color] {
+        return players.filter { $0.color != color }.map { $0.color }
     }
     
 }
