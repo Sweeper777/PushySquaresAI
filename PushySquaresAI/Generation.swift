f1bdac140352743709d53d464cd31a7c8dee6143
Sweeper777 <sumulang@gmail.com>
2017-11-02 07:11:41 +0000

    made the agents play against each other

--- PushySquaresAI/Generation.swift
+++ PushySquaresAI/Generation.swift
@@ -10,5 +10,20 @@ func runGeneration(previousFitness: Double?) -> Double {
     
     var i = 1
     for agent in allAgents {
+        for j in 0..<4 {
+            var availableAgents = allAgents.filter({ $0 != agent })
+            var agentPlayers = [
+                randomFromArrayAndRemove(&availableAgents),
+                randomFromArrayAndRemove(&availableAgents),
+                randomFromArrayAndRemove(&availableAgents),
+            ]
+            agentPlayers.insert(agent, at: j)
+            let game = AgentGame(agents: agentPlayers)
+            print("game \(i) starts")
+            game.run()
+            print("game \(i) ended")
+            fitnesses[agent]! += game.fitness[agent] ?? 0
+            i += 1
+        }
     }
 }
