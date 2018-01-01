ded1419889fb8b679a5453e6f1661a9b1d3e9e32
Sweeper777 <sumulang@gmail.com>
2018-01-01 11:22:07 +0800

    Revert "added makeAgentsPlayGame"
    This reverts commit a54ecfb30fef65dc7fd59b4ea732355ca26e02a6.

--- PushySquaresAI/AgentManager.swift
+++ PushySquaresAI/AgentManager.swift
@@ -13,41 +13,4 @@ class AgentManager {
         availableAgents = Array(realm.objects(Agent.self))
     }
     
-    func makeAgentsPlayGame(realm: Realm) {
-        
-        for agent in availableAgents {
-            if agent.life <= 0 {
-                if let index = availableAgents.index(of: agent) {
-                    availableAgents.remove(at: index)
-                }
-                if let index = breedableAgents.index(of: agent) {
-                    breedableAgents.remove(at: index)
-                }
-                try! realm.write {
-                    realm.delete(agent)
-                }
-            }
-        }
-        
-        while availableAgents.count >= 4 {
-            availableAgents = availableAgents.filter { !$0.isInvalidated }
-            
-            let agentPlayers = [
-                randomFromArrayAndRemove(&availableAgents),
-                randomFromArrayAndRemove(&availableAgents),
-                randomFromArrayAndRemove(&availableAgents),
-                randomFromArrayAndRemove(&availableAgents),
-                ]
-            try! realm.write {
-                for agent in agentPlayers {
-                    agent.life -= 1
-                    agent.repair()
-                }
-            }
-            let game = AgentGame(agents: agentPlayers)
-            
-            game.run(doPrint: false, realm: realm)
-        }
-    }
-    
 }
