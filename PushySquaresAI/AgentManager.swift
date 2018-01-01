08a60ddb3c6741ef54effb6e241be998c5ee4e8f
Sweeper777 <sumulang@gmail.com>
2018-01-01 11:22:19 +0800

    Revert "added properties"
    This reverts commit 443f698b2c453007e7565c238d343450fa59e9fe.

--- PushySquaresAI/AgentManager.swift
+++ PushySquaresAI/AgentManager.swift
@@ -1,10 +1,4 @@
 import RealmSwift
 
 class AgentManager {
-    var availableAgents: [Agent]
-    var breedableAgents: [Agent] = []
-//    var realm: Realm
-    var gamesPlayed = 0
-    
-    static let shared = AgentManager()
 }
