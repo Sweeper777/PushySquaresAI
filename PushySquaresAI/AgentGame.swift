ae13c94daf93c487194ead9aca7cd100d5538ff3
Mulang Su <sumulang@gmail.com>
2021-04-10 16:59:22 +0800

    changed fitness function

--- PushySquaresAI/AgentGame.swift
+++ PushySquaresAI/AgentGame.swift
@@ -1,47 +1,50 @@
 import Foundation
-class AgentGame: GameDelegate {
+class AgentGame {
     let colors: [Color: Agent]
-    var fitness: [Agent: Int]
+    var fitness: [Agent: Int] = [:]
+    var fitnessIfLose: [Agent: Int] = [:]
+    var fitnessIfWin: [Agent: Int] = [:]
     let agents: [Agent]
     let game: Game
     
     init(agents: [Agent], map: Map) {
         game = Game(map: map, playerCount: agents.count)
-        var tempColors: [Color: Agent] = [.color1: agents[0], .color3: agents[1]]
+        var tempColors: [Color: Agent] = [.red: agents[0], .green: agents[1]]
         if agents.count > 2 {
-            tempColors[.color2] = agents[2]
+            tempColors[.blue] = agents[2]
         }
         if agents.count > 3 {
-            tempColors[.color4] = agents[3]
+            tempColors[.yellow] = agents[3]
         }
         self.colors = tempColors
         self.agents = agents
-        self.fitness = [:]
-        game.delegate = self
     }
     
     func run(print: Bool = false) {
         let startDate = Date()
         while game.players.filter({ $0.lives > 0}).count >= 2 {
             let ai = GameAI(game: game.createCopy(), myColor: game.currentPlayer.color, agent: colors[game.currentPlayer.color]!)
+            let player = game.currentPlayer
             game.moveInDirection(ai.getNextMove())
             if print {
                 printGame(game)
             }
             if Date().timeIntervalSince(startDate) > 180 && !print {
+                Swift.print("Timed out!")
                 fitness = [:]
                 return
             }
-            fitness[colors[game.currentPlayer.color]!] = (fitness[colors[game.currentPlayer.color]!] ?? 0) + 1
+            fitnessIfLose[colors[player.color]!] =
+                    (fitnessIfLose[colors[player.color]!] ?? 0) + player.lives
+            fitnessIfWin[colors[player.color]!] =
+                    (fitnessIfWin[colors[player.color]!] ?? 1000) - 3
         }
-//        let remainingPlayers = game.players.filter({ $0.lives > 0})
-//        if remainingPlayers.count == 1 {
-//            fitness[colors[remainingPlayers.first!.color]!] = 3 * remainingPlayers.first!.lives
-//        }
-    }
-    
-    func playerDidMakeMove(direction: Direction?, originalPositions: [Position], destroyedSquarePositions: [Position], greyedOutPositions: [Position], newSquareColor: Color?) {
-        
+
+        let remainingPlayers = game.players.filter({ $0.lives > 0})
+        fitness = fitnessIfLose
+        if let winningPlayer = remainingPlayers.first {
+            fitness[colors[winningPlayer.color]!] = fitnessIfWin[colors[winningPlayer.color]!] ?? 1000
+        }
     }
     
     func playerDidDie(color: Color) {
