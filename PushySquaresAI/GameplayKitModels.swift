580eae30bbad799dde9aea8633afaeb0cadb2b71
Mulang Su <sumulang@gmail.com>
2021-04-14 11:00:26 +0800

    added GameModel

--- PushySquaresAI/GameplayKitModels.swift
+++ PushySquaresAI/GameplayKitModels.swift
@@ -1,3 +1,65 @@
+import GameplayKit
+
+class GameModel: NSObject, GKGameModel {
+    var game: Game
+
+    var myColor: Color {
+        game.currentPlayer.color
+    }
+
+    required init(game: Game) {
+        self.game = game
+        super.init()
+    }
+
+    var players: [GKGameModelPlayer]? = [PlayerModel(color: .red), PlayerModel(color: .green)]
+
+    var activePlayer: GKGameModelPlayer? {
+        if game.currentPlayer.color == .green {
+            return players![1]
+        } else {
+            return players![0]
+        }
+    }
+
+    func setGameModel(_ gameModel: GKGameModel) {
+        game = (gameModel as! GameModel).game.createCopy()
+    }
+
+    func gameModelUpdates(for player: GKGameModelPlayer) -> [GKGameModelUpdate]? {
+        [Direction.right, .up, .left, .down].map(MoveModel.init)
+    }
+
+    func apply(_ gameModelUpdate: GKGameModelUpdate) {
+        game.moveInDirection((gameModelUpdate as! MoveModel).move)
+    }
+
+    func copy(with zone: NSZone? = nil) -> Any {
+        type(of: self).init(game: game.createCopy())
+    }
+
+    func isLoss(for player: GKGameModelPlayer) -> Bool {
+        game.player(Color(rawValue: player.playerId)!).lives < 1
+    }
+
+    func isWin(for player: GKGameModelPlayer) -> Bool {
+        game.players.filter { $0.lives > 0 }.first?.color.rawValue == player.playerId
+    }
+
+//    func score(for player: GKGameModelPlayer) -> Int {
+//        let unclamped = evaluate(game, game.currentPlayer.color,
+//                9817,
+//                3256,
+//                2,
+//                6212,
+//                3272,
+//                4225,
+//                6744,
+//                2582,
+//                5886)
+//        return max(GKGameModelMinScore, min(unclamped, GKGameModelMaxScore))
+//    }
+}
 
 class PlayerModel: NSObject, GKGameModelPlayer {
     var playerId: Int {
